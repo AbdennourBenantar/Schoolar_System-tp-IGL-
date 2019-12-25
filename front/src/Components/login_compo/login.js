98f3f80d36069841c3648962cd314a17108b741f
meclotfi <40919923+meclotfi@users.noreply.github.com>
2019-12-25 16:53:33 +0100

    adding login component
    add

---
+++ front/src/Components/login_compo/login.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import "./login_style.css";
+import logo from "./grad.png";
+
+class Login extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      name: "",
+      password: ""
+    };
+    this.onchange = this.onchange.bind(this);
+    this.log_submit = this.log_submit.bind(this);
+  }
+  onchange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  log_submit(e) {
+    e.preventDefault();
+    console.log("hello world it is searj");
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="wrapper fadeInDown">
+        <div id="formContent">
+          <img className="fadeIn first" src={logo} id="icon" alt="User Icon" />
+          <h3>Easy School</h3>
+          <h2 className="active"> Sign In </h2>
+
+          <form onChange={this.onchange} onSubmit={this.log_submit}>
+            <input
+              type="text"
+              id="login"
+              className="fadeIn second"
+              name="name"
+              placeholder="login"
+              value={this.state.name}
+            />
+            <input
+              type="password"
+              id="password"
+              className="fadeIn third"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+            />
+            <input
+              id="submit_btn"
+              type="submit"
+              className="fadeIn fourth"
+              value="Log In"
+              onSubmit={this.log_submit}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+function OnSubmit() {}
+
+export default Login;
