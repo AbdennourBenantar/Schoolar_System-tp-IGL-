1bc0e3c636adbc178c00d1dd5962e217ff1841f4
meclotfi <40919923+meclotfi@users.noreply.github.com>
2019-12-25 17:06:25 +0100

    insert login in the beginning

--- front/src/Acceuil.js
+++ front/src/Acceuil.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Typical from 'react-typical';
 import ActCard from './Components/ActeursCard';
-
+import Login from './Components/login_compo/login'
 
 const acteurs=[
   {type:'Etudiant'},
@@ -11,21 +11,22 @@ const acteurs=[
 ];
 
 
-
+const sauv=(<header className="App-header">
+<Typical steps={['Bienvenue',1000,'Veuillez choisir votre profil : ',500]}/>
+<div className="ActCardsContainer">
+  <ActCard  acteur={acteurs[0]} />
+  <ActCard  acteur={acteurs[1]} />
+  <ActCard  acteur={acteurs[2]} />
+</div>
+</header>);
 
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <Typical steps={['Bienvenue',1000,'Veuillez choisir votre profil : ',500]}/>
-        <div className="ActCardsContainer">
-          <ActCard  acteur={acteurs[0]} />
-          <ActCard  acteur={acteurs[1]} />
-          <ActCard  acteur={acteurs[2]} />
-        </div>
-      </header>
+      <Login/>
+    
       </div>
   );};
 }
